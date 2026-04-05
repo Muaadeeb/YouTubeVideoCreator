f46f2a9a455ea0b2fea48e3702712f7a08333df0
Timothy Ingledue <TimothyIngledue@gmail.com>
2026-04-05 16:15:06 -0500

    Checkin #2 - app setup

--- AnimeStoryVideoCreatorApp/AnimeStoryVideoCreator/AnimeStoryVideoCreator.Client/Program.cs
+++ AnimeStoryVideoCreatorApp/AnimeStoryVideoCreator/AnimeStoryVideoCreator.Client/Program.cs
@@ -1,14 +1,22 @@
+using Blazorise;
+using Blazorise.Tailwind;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace AnimeStoryVideoCreator.Client
-{
-    internal class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+namespace AnimeStoryVideoCreator.Client;
 
-            await builder.Build().RunAsync();
-        }
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.Services
+            .AddBlazorise(options =>
+            {
+                options.Immediate = true;
+            })
+            .AddTailwindProviders();
+
+        await builder.Build().RunAsync();
     }
 }
