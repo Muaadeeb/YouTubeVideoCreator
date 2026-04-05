d6000d6bc28c1d282f954a4446a899b8cefa3563
Timothy Ingledue <TimothyIngledue@gmail.com>
2026-04-04 19:35:25 -0500

    Commit 1 - Solution Creation.

---
+++ AnimeStoryVideoCreatorApp/AnimeStoryVideoCreator/AnimeStoryVideoCreator.Client/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace AnimeStoryVideoCreator.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
